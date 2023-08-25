2589d9aa8f201d9de41c5fb2b1acbe68fe3611ba
DoDongssss <xchitts@gmail.com>
2023-08-25 11:13:49 +0800

    path

--- src/components/taglineBox.vue
+++ src/components/taglineBox.vue
@@ -1,6 +1,11 @@
 <template>
    <div
-      class="absolute top-[-50px] z-10 flex w-full items-center justify-center"
+      class="absolute top-[-50px] z-10 flex w-full items-center justify-center transition-all"
+      :class="
+         scrollPost >= 100
+            ? 'opacity-1 translate-y-0'
+            : 'translate-y-10 opacity-0'
+      "
    >
       <div class="max-w-[1000px]">
          <h1
@@ -14,9 +19,24 @@
 </template>
 
 <script>
+import { ref } from "vue"
+import { translate } from "element-plus"
+
 export default {
    setup() {
-      return {}
+      const scrollPost = ref()
+      return { scrollPost }
+   },
+   methods: {
+      scroll() {
+         this.scrollPost = window.scrollY || window.pageYOffset
+      },
+   },
+   mounted() {
+      window.addEventListener("scroll", this.scroll)
+   },
+   beforeDestroy() {
+      window.removeEventListener("scroll", this.scroll)
    },
 }
 </script>
